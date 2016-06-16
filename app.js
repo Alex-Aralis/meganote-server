890653258a6eb3367aa15403a6ef1a53a8e0d3ad
Alex Aralis <alex.aralis@gmail.com>
2016-06-16 10:47:29 +0000

    add npm start and make app.js serve something when root is requested

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('hallu');
+});
+
+app.listen(3030, function(){
+    console.log('listening on port 3030');
+});
+
 
